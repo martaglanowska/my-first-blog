e448d74962f2787ea90a335ca655a6669d665d76
Marta <marta.glanowska@outlook.com>
2019-12-26 22:06:37 +0100

    My Django Girls app, first commit

---
+++ abc3.py
@@ -0,0 +1,5 @@
+a = 5+3
+print(a)
+slownik = {}
+slownik["imie"] = "Marta"
+print(slownik["imie"])
---
+++ srodowisko/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/marta/djangogirls/srodowisko/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
